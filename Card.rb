33ef38a7641960abf1cb40a946a0cd0d7ede1567
Ryan Lesson & Salim Rahimi <pair+Bigless27.srahimi1@devbootcamp.com>
2015-10-30 17:31:39 -0400

    Finish Zombie Flash cards

--- Card.rb
+++ Card.rb
@@ -1,1 +1,14 @@
+class Card
+  attr_reader :definition, :answer
+
+  def initialize(args={})
+    @definition=args[:definition]
+    @answer=args[:answer]
+  end
+
+  def check?(guess)
+    @answer == guess.strip.downcase
+  end
+
+end
 
